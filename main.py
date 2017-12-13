eb1fbed4e71972cfc6850cd94b7daf854483d1fc
paulo <paulo.fzmz@gmail.com>
2017-12-12 22:37:04 -0300

    version server

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import general_functions as gf, numpy as np, gc, PBL, PBL_stern
 mol_name = '5pti'
 # PBL_stern.solvation_energy(mol_name, 2., info=True)
 
-dens = np.array([ 8., 16., 32., 64. ])
+dens = np.array([ 2., 4., 8., 16., 32., 64. ])
 
-for dd in dens:
-	PBL.solvation_energy(mol_name, dd, info=True)
- 	gc.collect()
- 	PBL.solvation_energy(mol_name, dd, formulation='juffer', info=True)
- 	gc.collect()
- 	PBL_stern.solvation_energy(mol_name, dd, info=True)
- 	gc.collect()
- 	PBL_stern.solvation_energy(mol_name, dd, formulation='asc', info=True)
- 	gc.collect()
-	gf.run_pygbe(mol_name, dd, 1.4, info=True)
- 	gc.collect()
+#for dd in dens:
+#	PBL.solvation_energy(mol_name, dd, info=True)
+# 	gc.collect()
+# 	PBL.solvation_energy(mol_name, dd, formulation='juffer', info=True)
+# 	gc.collect()
+# 	PBL_stern.solvation_energy(mol_name, dd, info=True)
+# 	gc.collect()
+# 	PBL_stern.solvation_energy(mol_name, dd, formulation='asc', info=True)
+# 	gc.collect()
+#	gf.run_pygbe(mol_name, dd, 1.4, info=True)
+# 	gc.collect()
 
 
 r_st = np.array([ .2, .4, .6, .8, 1.2, 1.5, 2., 3., 4. ])
